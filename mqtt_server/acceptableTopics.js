40ae7b2b5a0941f09c138563b6cc4339ece20d8c
mbool <mstybowski@gmail.com>
2021-01-11 17:39:39 +0100

    Added new topic to acceptable topics

--- mqtt_server/acceptableTopics.js
+++ mqtt_server/acceptableTopics.js
@@ -1,6 +1,6 @@
 module.exports = {
     request: ["request/state", "request/mode", "request/files", "request/onlineInterface", "request/interfacesConfiguration"],
-    command: ["command/useMode", "command/gesture", "command/startRecording", "command/runScript"],
+    command: ["command/useMode", "command/gesture", "command/startRecording", "command/runScript", "command/controlSensor"],
     interfaces: ["interfaces/add", "interfaces/edit", "interfaces/remove", "interfaces/use", "interfaces/end"],
     sensors: ["sensors/data", "sensors/control", "sensors/log"]
   }
