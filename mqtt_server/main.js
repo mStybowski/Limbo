81e2ceffc5d68873f6bca53f21eff05d1ed1d794
mbool <39348860+mStybowski@users.noreply.github.com>
2021-01-22 12:16:57 +0100

    Update main.js

--- mqtt_server/main.js
+++ mqtt_server/main.js
@@ -285,7 +285,7 @@ class MQTTClient{
 
         let messageObject = {}
         try{
-            messageObject = JSON.parse(message.toString())
+            messageObject = JSON.parse(message)
         }
         catch{
             return;
@@ -331,7 +331,7 @@ class MQTTClient{
     postClassifier(message){
         let messageObject = {}
         try{
-            messageObject = JSON.parse(message.toString())
+            messageObject = JSON.parse(message)
         }
         catch{
             this.serverLogs("Classifier returned invalid JSON.", "warning", true)
@@ -352,7 +352,7 @@ class MQTTClient{
     postFineTune(message){
         let messageObject = {}
         try {
-            messageObject = JSON.parse(message.toString())
+            messageObject = JSON.parse(message)
         }
         catch{
             this.serverLogs("Fine_tuner returned invalid JSON.", "warning", true)
